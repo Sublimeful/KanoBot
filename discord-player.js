b56b801f8b9a115d2c24f0a556a89e46231b0702
Sublimeful <Qiangwu2000@gmail.com>
2021-07-12 05:34:07 -0500

    literally EVERYTHING

--- discord-player.js
+++ discord-player.js
@@ -1,94 +1,420 @@
 const ytdl = require("ytdl-core");
+const yts = require('yt-search');
 const fetch = require("node-fetch")
+const ffmpeg = require('fluent-ffmpeg');
+const EventEmitter = require('events');
 
-class Player {
+
+
+class Player extends EventEmitter {
   constructor(client) {
+    super();
     this.servers = {};
     this.client = client;
   }
+
+  /* Getter for currentTrack */
+  getCurrentTrack(message) {
+    const server = this.servers[message.guild.id];
+
+    return server ? server.currentTrack : null;
+  }
+
+  /* Getter for server queue */
+  getQueue(message) {
+    const server = this.servers[message.guild.id];
+
+    return server ? server.queue : null;
+  }
+
+  /* Create a contract with the server */
+  createContract(message) {
+    if(!this.servers[message.guild.id]) {
+      this.servers[message.guild.id] = {
+        queue: [],
+        currentTrack: -1,
+        connection: null,
+        volume: 1.0,
+        isPlaying: false,
+        loop: "off"
+      }
+    }
+  }
+
+  /* Get the track Object from a query */
+  async #getTrackObject(message, query) {
+    const track = {
+      title: "Unknown Title",
+      url: query,
+      id: null,
+      duration: null,
+      thumbnail: null,
+      requestor: message.author.toString()
+    }
+
+    if(ytdl.validateURL(query)) {
+      // Tests if query is a Youtube link
+      const ytdlInfo = await ytdl.getInfo(query);
+      const videoDetails = ytdlInfo.videoDetails;
+
+      track.title = videoDetails.title;
+      track.id = videoDetails.videoId;
+      track.duration = parseInt(videoDetails.lengthSeconds);
+      track.thumbnail = videoDetails.thumbnails[videoDetails.thumbnails.length - 1].url;
+
+      return track;
+    }
+
+    try {
+      // Tests if query is a media link; else it is a search query
+      const res = await fetch(query);
+      const contentType = res.headers.get("Content-Length");
+      if(contentType) {
+        return await new Promise((resolve, _) => {
+          ffmpeg.ffprobe(query, (_, metadata) => {
+            track.duration = Math.floor(metadata.format.duration + 0.5);
+            resolve(track);
+          })
+        })
+      }
+    } catch(err) {}
+
+    // Searching notification
+    this.emit("notification", message, "search", query);
+
+    // Search for the query on Youtube, play first result
+    const search = await yts(query);
+
+    if(search.videos.length > 0) {
+      const video = search.videos[0];
+
+      track.title = video.title;
+      track.url = video.url;
+      track.id = video.videoId;
+      track.duration = video.seconds;
+      track.thumbnail = video.thumbnail;
+
+      return track;
+    }
+  }
+
+  /* Jump to a specific track position, returns success */
+  async jump(message, trackPosition) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.queue[trackPosition]) {
+      server.currentTrack = trackPosition;
+      await this.play(message, server.queue[trackPosition]);
+      return true;
+    }
+
+    return false;
+  }
+
+  /* Remove from "from" to "to" in queue, pretty self explanatory */
+  async remove(message, from, to) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.queue[from] && server.queue[to]) {
+      const currentTrack = server.queue[server.currentTrack];
+      const deletedTracks = server.queue.splice(Math.min(from, to), Math.abs(from - to) + 1);
+
+      // Notify the user that a change has occured!
+      this.emit("notification", message, "remove", deletedTracks);
+
+      if(Math.min(from, to) < server.currentTrack) {
+        const min = Math.min(from, to);
+        const max = Math.max(from, to);
+        const clamp = Math.min(server.currentTrack, max + 1);
+        server.currentTrack -= clamp - min;
+      }
+      if(deletedTracks.includes(currentTrack)) {
+        if(await this.jump(message, server.currentTrack)) return;
+        if(await this.jump(message, server.queue.length - 1)) return;
+
+        server.currentTrack = -1;
+        await this.stop(message);
+      }
+    }
+  }
+
+  /* Clears the queue */
+  async clear(message) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.queue.length > 0) {
+      server.queue = [];
+
+      // Notify the user that a change has occured!
+      this.emit("notification", message, "clear");
+
+      server.currentTrack = -1;
+      await this.stop(message);
+    }
+  }
+
+  /* Moves the track at position "from" to position "to" */
+  async move(message, from, to) {
+    if(to < 0) return;
+
+    const server = this.servers[message.guild.id];
+    
+    if(server && server.queue[from]) {
+      // Notify the user that a change has occured!
+      this.emit("notification", message, "move", [server.queue[from], from + 1, to + 1]);
+
+      server.queue.splice(to, 0, ...server.queue.splice(from, 1));
+
+      if(server.currentTrack == from) {
+        server.currentTrack = to;
+      } else if(from > server.currentTrack && to <= server.currentTrack) {
+        server.currentTrack++;
+      } else if(from < server.currentTrack && to >= server.currentTrack) {
+        server.currentTrack--;
+      }
+    }
+  }
+
 
   /* Establish a connection to the user's call, and add the server to the server list */
   async join(message) {
     // return FALSE if the user is not in a VC or if there is no permissions
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
-      message.channel.send("You need to be in a voice channel to play music!");
+      this.emit("error", message, "voiceChannel");
       return false;
     }
 
     const permissions = voiceChannel.permissionsFor(message.client.user);
     if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-      message.channel.send("I need the permissions to join and speak in your voice channel!");
+      this.emit("error", message, "permissions");
       return false;
     }
 
-    // Create the contract for the server
-    if(!this.servers[message.guild.id]) {
-      this.servers[message.guild.id] = {
-        queue: {},
-        connection: null,
-        volume: 1.0
-      }
-    }
+    // Create a contract with the server (one time function)
+    this.createContract(message);
 
     // Attempt to join the VC and establish a connection
+    // ; If already in the VC, then don't join again and return true
     const server = this.servers[message.guild.id];
+
+    if(server.connection && voiceChannel == server.connection.channel) return true;
+
     try {
       const connection = await voiceChannel.join();
       server.connection = connection;
 
       // When the connection has been cut(or disconnected), set the connection to null
+      // and isPlaying status to false
       server.connection.on("disconnect", () => {
         server.connection = null;
+        server.isPlaying = false;
+        this.emit("notification", message, "disconnected");
       })
 
       return true;
     } catch(err) {
       console.log(err);
+
       return false;
     }
   }
 
-  async play(message, query) {
+  /* Skips current track and attempts to play next track, returns success */
+  async skip(message) {
+    // Check if there is a next track in queue
+    // ; If there is a next track, then increment currentTrack by one and play next track
+    // ; If there is no next track, then stop playback and leave the call
+    const server = this.servers[message.guild.id];
+
+    if(server && await this.jump(message, server.currentTrack + 1)) return true;
+
+    server.currentTrack = server.queue.length;
+    await this.stop(message);
+    return false;
+  }
+
+  /* Skips to the previous track, returns success */
+  async prev(message) {
+    // Same thing as skip but reverse
+    const server = this.servers[message.guild.id];
+
+    if(server && await this.jump(message, server.currentTrack - 1)) return true;
+
+    server.currentTrack = -1;
+    await this.stop(message);
+    return false;
+  }
+
+  /* Stops playback and exits the VC */
+  async stop(message) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.connection) {
+      server.connection.disconnect();
+    }
+  }
+
+  /* Set loop options */
+  async loop(message, option) {
+    const server = this.servers[message.guild.id];
+    if(server) {
+      switch(option) {
+        case "track":
+          server.loop = "track";
+          this.emit("notification", message, "loop", "track");
+          break;
+        case "queue":
+          server.loop = "queue";
+          this.emit("notification", message, "loop", "queue");
+          break;
+        default:
+          server.loop = "off";
+          this.emit("notification", message, "loop", "off");
+      }
+    }
+  }
+
+  /* Play's the track immediately and without question */
+  async play(message, track) {
     // Don't play anything if is not in VC
     const isConnected = await this.join(message);
     if(!isConnected) return;
 
     const server = this.servers[message.guild.id];
 
+    server.connection
+      .play(ytdl.validateURL(track.url) ? ytdl(track.url, {filter : 'audioonly'}) : track.url)
+      .on("finish", () => {
+        if(server.loop == "track")
+          return this.jump(message, server.currentTrack);
+        if(server.currentTrack == server.queue.length - 1 && server.loop == "queue")
+          return this.jump(message, 0);
+        this.skip(message);
+      })
+      .on("error", err => {
+        this.skip(message);
+        console.log(err);
+      })
 
-    // Is Youtube link
-    if(ytdl.validateURL(query)) {
-      
-    } else {
-      // Tests if query is a media link; else it is just a normal query
-      var isMedia = false;
+    // Set the volume of the playback
+    server.connection.dispatcher.setVolumeLogarithmic(server.volume);
 
-      try {
-        const res = await fetch(query);
-        const contentType = res.headers.get("Content-Type");
-        if(contentType) {
-          isMedia = true;
-        }
-      } catch(err) {}
+    server.isPlaying = true;
+    this.emit("notification", message, "trackStart", track);
+  }
 
-      if(isMedia) {
-        server.connection.play(query);
+  /* Pause playback */
+  async pause(message) {
+    const server = this.servers[message.guild.id];
 
-        
-      } else {
-        
+    if(server && server.isPlaying) {
+      server.connection.dispatcher.pause();
+      this.emit("notification", message, "pause");
+    }
+  }
+
+  /* Resume playback */
+  async resume(message) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.isPlaying) {
+      server.connection.dispatcher.resume();
+      this.emit("notification", message, "resume");
+    }
+  }
+
+  /* Seek to x ms in time */
+  async seekTo(message, ms) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.isPlaying) {
+      const track = server.queue[server.currentTrack];
+      const stream = ytdl.validateURL(track.url) ? ytdl(track.url, {filter : 'audioonly'}) : track.url;
+      ms = ms < 0 ? 0 : (ms > track.duration * 1000 ? track.duration * 1000 : ms);
+
+      server.connection
+        .play(stream, {seek: ms/1000})
+        .on("finish", () => {
+          if(server.loop == "track")
+            return this.jump(message, server.currentTrack);
+          if(server.currentTrack == server.queue.length - 1 && server.loop == "queue")
+            return this.jump(message, 0);
+          this.skip(message);
+        })
+        .on("error", err => {
+          this.skip(message);
+          console.log(err);
+        })
+
+      // Set seek time
+      server.connection.dispatcher.seek = ms;
+
+      // Set the volume of the playback
+      server.connection.dispatcher.setVolumeLogarithmic(server.volume);
+
+      this.emit("notification", message, "seekTo", ms);
+    }
+  }
+
+  /* Seek "ms" milliseconds forward or backwards */
+  async seek(message, ms) {
+    const server = this.servers[message.guild.id];
+
+    if(server && server.isPlaying) {
+      const st = server.connection.dispatcher.streamTime;
+      const s = server.connection.dispatcher.seek;
+
+      await this.seekTo(message, st + ms + (s ? s : 0));
+    }
+  }
+
+  /* Set volume (1.0 is 100%, 0.5 is 50%, etc.) */
+  async setVolume(message, volume) {
+    const server = this.servers[message.guild.id];
+
+    if(server) {
+      server.volume = volume;
+      this.emit("notification", message, "setVolume", volume);
+      if(server.isPlaying) {
+        server.connection.dispatcher.setVolumeLogarithmic(server.volume);
       }
     }
+  }
+
+  /* Adds a track to the queue */
+  async enqueue(message, query) {
+    const server = this.servers[message.guild.id];
+    const track = await this.#getTrackObject(message, query);
+
+    if(track != null) {
+      server.queue.push(track);
+      this.emit("notification", message, "trackAdded", track);
+    } else {
+      this.emit("notification", message, "noResults", query);
+    }
+  }
       
+  /* Play/Enqueue hybrid function for "play" command */
+  async execute(message, query) {
+    // Don't do anything if is not in VC
+    const isConnected = await this.join(message);
+    if(!isConnected) return;
 
-      
-    
+    const server = this.servers[message.guild.id];
 
+    // Enqueue the track; if nothing is playing, then play the last track in queue
+    await this.enqueue(message, query);
 
-
-
+    if(server.isPlaying == false) {
+      // Plays the last track in queue, disconnects if queue is empty
+      if(await this.jump(message, server.queue.length - 1)) return;
+      await this.stop(message);
+    }
   }
 }
 
+
+
 module.exports = Player;
+
--- index.js
+++ index.js
@@ -1,12 +1,79 @@
 const { Client } = require('discord.js');
 const { prefix, token } = require("./config");
+const { formatTime, codify, getPrintableQueue, getNowPlaying, getSimpleEmbed, getMoveEmbed, getSong, getHelp } = require("./formatter");
+
 const Player = require("./discord-player");
 
 const client = new Client();
 const player = new Player();
 
-//player.on("trackStart", (message, track) => message.channel.send(`Now playing ${track.title}...`));
+var processor;
 
+
+
+//<-- event: onnotification
+player.on("notification", (message, type, data) => {
+  switch(type) {
+    case "trackStart":
+      message.channel.send(getNowPlaying(data));
+      break;
+    case "trackAdded":
+      message.channel.send(getSimpleEmbed(`Track added: [${data.title}](${data.url}) [${data.requestor}]`));
+      break;
+    case "disconnected":
+      message.channel.send(getSimpleEmbed("🛑 Playback has been stopped because I have disconnected!"))
+      break;
+    case "clear":
+      message.channel.send(getSimpleEmbed("🗑️ Cleared the queue!"))
+      break;
+    case "remove":
+      message.channel.send(getSimpleEmbed(`${data.length} songs have been removed! These are the songs:`))
+      message.channel.send(getPrintableQueue(data));
+      break;
+    case "move":
+      message.channel.send(getMoveEmbed(data[0], data[1], data[2]));
+      break;
+    case "search":
+      message.channel.send(getSimpleEmbed(`**----- Searching for -----** ${codify(data)}`));
+      break;
+    case "noResults":
+      message.channel.send(getSimpleEmbed(`No results could be found for search query: ${codify(data)}`));
+      break;
+    case "pause":
+      message.channel.send(getSimpleEmbed("⏸ Playback has been paused!"))
+      break;
+    case "resume":
+      message.channel.send(getSimpleEmbed("▶️ Playback has been resumed!"))
+      break;
+    case "seekTo":
+      message.channel.send(getSimpleEmbed(`**----- Seeked to -----** ${codify(formatTime(Math.floor(data/1000)))}`));
+      break;
+    case "setVolume":
+      message.channel.send(getSimpleEmbed(`**----- New volume -----** ${codify(`${Math.floor(data * 100)}%`)}`));
+      break;
+    case "loop":
+      message.channel.send(getSimpleEmbed(`**----- Loop -----** ${codify(data)}`));
+      break;
+    default:
+      message.channel.send(getSimpleEmbed("How is this even possible?"));
+  }
+})
+//-->
+
+//<-- event: onerror
+player.on("error", (message, reason) => {
+  switch(reason) {
+    case "voiceChannel":
+      message.channel.send(getSimpleEmbed("You need to be in a voice channel to play music!"));
+      break;
+    case "permissions":
+      message.channel.send(getSimpleEmbed("I need the permissions to join and speak in your voice channel!"));
+      break;
+    default:
+      message.channel.send(getSimpleEmbed("How the fuck did this even happen?"));
+  }
+})
+//-->
 
 //<-- event: onmessage
 client.on("message", async message => {
@@ -15,13 +82,87 @@ client.on("message", async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  switch(command) {
-    case "play":
-      player.play(message, args[0]);
-      break;
-    default:
-      message.channel.send("```You need to enter a valid command!```");
-  }
+  const serverQueue = player.getQueue(message);
+  const serverCurrentTrack = player.getCurrentTrack(message);
+
+  if(serverQueue == null) player.createContract(message);
+
+  // Waits until the previous command is fully processed before continuing
+  await processor;
+
+  processor = new Promise(async (resolve, _) => {
+    switch(command) {
+      case "help":
+        message.channel.send(getHelp(message, client, args));
+        break;
+      case "play":
+        await player.execute(message, args.join(" "));
+        break;
+      case "nowplaying":
+        if(!serverQueue) {
+          message.channel.send(getNowPlaying(null));
+          break;
+        }
+        message.channel.send(getNowPlaying(serverQueue[serverCurrentTrack]));
+        break;
+      case "song":
+        if(!serverQueue) {
+          message.channel.send(getSong(null));
+          break;
+        }
+        message.channel.send(getSong(serverQueue[parseInt(args[0]) - 1]));
+        break;
+      case "queue":
+        message.channel.send(getPrintableQueue(serverQueue, serverCurrentTrack));
+        break;
+      case "skip":
+        await player.skip(message);
+        break;
+      case "prev":
+        await player.prev(message);
+        break;
+      case "stop":
+        await player.stop(message);
+        break;
+      case "jump":
+        await player.jump(message, parseInt(args[0]) - 1);
+        break;
+      case "remove":
+        const a1 = parseInt(args[0]) - 1;
+        const a2 = parseInt(args[1]) - 1;
+        await player.remove(message, a1, args.length < 2 ? a1 : a2);
+        break;
+      case "clear":
+        await player.clear(message);
+        break;
+      case "move":
+        await player.move(message, parseInt(args[0]) - 1, parseInt(args[1]) - 1);
+        break;
+      case "pause":
+        await player.pause(message);
+        break;
+      case "resume":
+        await player.resume(message);
+        break;
+      case "volume":
+        await player.setVolume(message, parseFloat(args[0]));
+        break;
+      case "seekto":
+        await player.seekTo(message, Math.floor(parseFloat(args[0]) * 1000));
+        break;
+      case "seek":
+        await player.seek(message, Math.floor(parseFloat(args[0]) * 1000));
+        break;
+      case "loop":
+        await player.loop(message, args[0]);
+        break;
+      default:
+        message.channel.send(getSimpleEmbed(`Please provide a valid command! [${message.author.toString()}]`));
+    }
+
+    // Resolve after the switch statement is complete, meaning command has been carried out
+    resolve();
+  })
 });
 //-->
 
@@ -47,8 +188,6 @@ client.once("ready", () => {
 //-->
 
 client.login(token);
-
-
 
 
 
