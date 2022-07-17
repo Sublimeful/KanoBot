8620f782d68da0c3bfb569e77656695cbd715261
Sublimeful <sublimeful@Chronos.attlocal.net>
2022-07-17 03:27:07 -0500

    made dbeditor fields required

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   prefix: "~",
-  token: "ODYyODY0MDUyODMxOTExOTc3.GyZrqX.DM2cl2Tc3L1Ele8b9_lT48jhT1I_WvPmrkeO1E",
+  token: "ODYyODY0MDUyODMxOTExOTc3.GN7NZg.0_JLDE-2l_WiT2PEEMutETBfGyVuxcT3eDhpNw",
   port: 32400
 }
 
